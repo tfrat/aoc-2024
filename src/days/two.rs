1d10367a8459ab868a1c8e8cbe6a6aa65ba50f70
Tim Fratangelo <tfrat84@gmail.com>
2024-12-01 21:54:16 -0500

    Added second day

---
+++ src/days/two.rs
@@ -0,0 +1,15 @@
+use std::fmt::Display;
+use crate::days::Day;
+
+#[derive(Default)]
+struct DayTwo {}
+
+impl Day for DayTwo {
+    fn part_one(&self, input: &str) -> Box<dyn Display> {
+        todo!()
+    }
+
+    fn part_two(&self, input: &str) -> Box<dyn Display> {
+        todo!()
+    }
+}
