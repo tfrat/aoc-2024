12ca7c4206ce361a68f46d94e1a57dcf374aa04a
Tim Fratangelo <tfrat84@gmail.com>
2024-12-01 20:58:59 -0500

    Added some light polymorphism

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,39 +1,45 @@
 use std::collections::HashMap;
+use std::fmt::Display;
+use crate::days::Day;
 
-fn number_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
-    let lines: Vec<&str> = input.lines().collect();
-    let mut left: Vec<i32> = Vec::with_capacity(lines.len());
-    let mut right: Vec<i32> = Vec::with_capacity(lines.len());
-    for line in lines {
-        let numbers: Vec<&str> = line.split_whitespace().collect();
-        left.push(numbers[0].parse().expect(""));
-        right.push(numbers[1].parse().expect(""));
+#[derive(Default)]
+pub struct DayOne { }
+impl DayOne {
+    fn number_lists(input: &str) -> (Vec<i32>, Vec<i32>) {
+        let lines: Vec<&str> = input.lines().collect();
+        let mut left: Vec<i32> = Vec::with_capacity(lines.len());
+        let mut right: Vec<i32> = Vec::with_capacity(lines.len());
+        for line in lines {
+            let numbers: Vec<&str> = line.split_whitespace().collect();
+            left.push(numbers[0].parse().expect(""));
+            right.push(numbers[1].parse().expect(""));
+        }
+        (left, right)
     }
-    (left, right)
 }
 
-pub fn part_one(input: &str) -> i32 {
-    let (mut left, mut right) = number_lists(input);
-    left.sort();
-    right.sort();
-    let mut sum = 0;
-    for (left_item, right_item) in left.into_iter().zip(right.into_iter()) {
-        sum += (left_item - right_item).abs()
-    }
-    sum
-}
+impl Day for DayOne {
+    fn part_one(&self, input: &str) -> impl Display {
+        let (mut left, mut right) = DayOne::number_lists(input);
+        left.sort();
+        right.sort();
+        let mut sum = 0;
+        for (left_item, right_item) in left.into_iter().zip(right.into_iter()) {
+            sum += (left_item - right_item).abs()
+        }
+        sum    }
 
-pub fn part_two(input: &str) -> i32 {
-    let (left, right) = number_lists(input);
-    let mut similarity_score = 0;
-    let mut right_count: HashMap<i32, i32> = HashMap::new();
+    fn part_two(&self, input: &str) -> impl Display {
+        let (left, right) = DayOne::number_lists(input);
+        let mut similarity_score = 0;
+        let mut right_count: HashMap<i32, i32> = HashMap::new();
 
-    for right_item in right {
-        *right_count.entry(right_item).or_insert(0) += 1;
-    }
-    for left_item in left {
-        let count = *right_count.get(&left_item).get_or_insert(&0);
-        similarity_score += left_item * count;
-    }
-    similarity_score
+        for right_item in right {
+            *right_count.entry(right_item).or_insert(0) += 1;
+        }
+        for left_item in left {
+            let count = *right_count.get(&left_item).get_or_insert(&0);
+            similarity_score += left_item * count;
+        }
+        similarity_score    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod days;
 
+use std::fmt::Display;
 use std::fs;
 use std::process;
 use clap::{command, Parser};
+use crate::days::Day;
 
 #[derive(clap::ValueEnum, Clone, Eq, PartialEq)]
 enum Part {
@@ -20,27 +22,12 @@ struct Args {
     /// Which part to execute
     #[arg(short, long, value_enum)]
     part: Part,
-    /// Use the example input?
-    #[arg(short, long, action)]
-    example: bool,
+    #[arg(short, long, value_enum)]
+    filename: String,
 }
 
-fn get_input(day: &u8, part: &Part, example: &bool) -> String {
-    let filename = if *example {
-        "example.txt"
-    } else {
-        if part == &Part::PartOne {
-            "part_one.txt"
-        } else {
-            "part_two.txt"
-        }
-    };
-    let path = format!("inputs/day_{}/{}", day, &filename);
-
-    match fs::exists(path) {
-        Result::Ok(bool) => ,
-
-    }
+fn get_input(day: &u8, filename: &str) -> String {
+    let path = format!("inputs/day_{}/{}", day, filename);
 
     match fs::read_to_string(&path) {
         Ok(contents) => {
@@ -54,12 +41,13 @@ fn get_input(day: &u8, part: &Part, example: &bool) -> String {
 }
 
 fn main() {
-
     let args: Args = Args::parse();
-    let input = get_input(&args.day, &args.part, &args.example);
-    let answer = match &args.part {
-        &Part::PartOne => days::day_1::part_one(&input),
-        &Part::PartTwo => days::day_1::part_two(&input),
+
+    let input = get_input(&args.day, &args.filename);
+    let day_one = days::day_1::DayOne::default();
+    let answer: Box<dyn Display> = match &args.part {
+        &Part::PartOne => Box::new(day_one.part_one(&input)),
+        &Part::PartTwo => Box::new(day_one.part_two(&input)),
     };
-    println!("{}\n", answer)
+    println!("{}", answer)
 }
