d0ea04ee8ace52aa7e064914592137ea42e6ca8c
Tim Fratangelo <tfrat84@gmail.com>
2024-12-02 19:03:38 -0500

    Fixed part two

--- src/days.rs
+++ src/days.rs
@@ -13,7 +13,7 @@ pub fn get_day(day: &u8, part: &Part) -> Result<Box<dyn Day>, String> {
     match (day, part) {
         (1, _) => Ok(Box::new(one::DayOne {})),
         (2, Part::PartOne) => Ok(Box::new(two::DayTwo {
-            dampener: 0,
+            enable_dampener: false,
             ..Default::default()
         })),
         (2, Part::PartTwo) => Ok(Box::new(two::DayTwo::default())),
