14f4f426e35a9514fbc8f0caa10ad869415bd4bc
Tim Fratangelo <tfrat84@gmail.com>
2024-12-04 14:00:15 -0500

    Day Four part one

--- src/days/four.rs
+++ src/days/four.rs
@@ -1,37 +1,82 @@
 use crate::days::Day;
-use regex::Regex;
+use std::collections::HashMap;
+
+struct Grid {
+    grid: HashMap<(i32, i32), char>,
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Grid {
+    fn new(input: &str) -> Grid {
+        let mut grid = HashMap::new();
+        for (y, line) in input.lines().enumerate() {
+            for (x, value) in line.chars().enumerate() {
+                grid.insert((x as i32, y as i32), value);
+            }
+        }
+        let height = input.lines().collect::<Vec<_>>().len() as u32;
+        let width: u32 = input.lines().collect::<Vec<_>>()[0].len() as u32;
+        Grid {
+            grid,
+            height,
+            width,
+        }
+    }
+
+    fn get(&self, x: i32, y: i32) -> Option<&char> {
+        self.grid.get(&(x, y))
+    }
+}
 
 #[derive(Default)]
 pub struct DayFour {}
 
 impl DayFour {
-    fn count_xmas(line: &str) -> u32 {
-        let re = Regex::new(r"XMAS|SAMX").unwrap();
-        re.find_iter(line).count() as u32
+    fn check_all_directions(grid: &Grid, x: i32, y: i32, xmas: &str) -> u32 {
+        (-1..=1)
+            .map(|x_offset| {
+                (-1..=1)
+                    .map(|y_offset| match (x_offset, y_offset) {
+                        (0, 0) => 0,
+                        _ => Self::count_xmas(
+                            grid,
+                            x + x_offset,
+                            y + y_offset,
+                            x_offset,
+                            y_offset,
+                            xmas,
+                        ),
+                    })
+                    .sum::<u32>()
+            })
+            .sum()
     }
 
-    fn get_lines(grid: &str) -> Vec<String> {
-        let horizontal: Vec<String> = grid.lines().map(|line| line.to_string()).collect();
-        let vertical: Vec<String> = (0..horizontal[0].len())
-            .map(|i| {
-                horizontal
-                    .iter()
-                    .map(|l| &l[i..i + 1])
-                    .collect::<Vec<&str>>()
-                    .join("")
-            })
-            .collect();
-
-        [horizontal, vertical].concat()
+    fn count_xmas(grid: &Grid, x: i32, y: i32, x_offset: i32, y_offset: i32, xmas: &str) -> u32 {
+        match (grid.get(x, y), xmas) {
+            (Some('X'), "") => Self::check_all_directions(grid, x, y, "X"),
+            (Some('M'), "X") => {
+                Self::count_xmas(grid, x + x_offset, y + y_offset, x_offset, y_offset, "XM")
+            }
+            (Some('A'), "XM") => {
+                Self::count_xmas(grid, x + x_offset, y + y_offset, x_offset, y_offset, "XMA")
+            }
+            (Some('S'), "XMA") => 1,
+            _ => 0,
+        }
     }
 }
 
 impl Day for DayFour {
     fn part_one(&self, input: &str) -> String {
-        let lines = Self::get_lines(input);
-        lines
-            .iter()
-            .map(|line| Self::count_xmas(line))
+        let grid = Grid::new(input);
+        (0..grid.width)
+            .map(|x| {
+                (0..grid.height)
+                    .map(|y| Self::count_xmas(&grid, x as i32, y as i32, 0, 0, ""))
+                    .sum::<u32>()
+            })
             .sum::<u32>()
             .to_string()
     }
@@ -48,8 +93,17 @@ pub mod test {
     #[test]
     fn test_part_one() {
         let day = DayFour::default();
-        let cases = vec![(
-            r#"MMMSXXMASM
+        let cases = vec![
+            (
+                r#"..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X...."#,
+                4,
+            ),
+            (
+                r#"MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -59,8 +113,9 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"#,
-            18,
-        )];
+                18,
+            ),
+        ];
         for (input, expected) in cases {
             assert_eq!(day.part_one(input), expected.to_string())
         }
