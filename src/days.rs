fdf1a7ed20ad6d3476f90d1df09eae010b83c30b
Tim Fratangelo <tfrat84@gmail.com>
2024-12-03 22:58:58 -0500

    Added day four starter

--- src/days.rs
+++ src/days.rs
@@ -1,5 +1,6 @@
 use crate::Part;
 
+mod four;
 pub mod one;
 mod three;
 mod two;
@@ -18,6 +19,7 @@ pub fn get_day(day: &u8, part: &Part) -> Result<Box<dyn Day>, String> {
         })),
         (2, Part::PartTwo) => Ok(Box::new(two::DayTwo::default())),
         (3, _) => Ok(Box::new(three::DayThree::default())),
+        (4, _) => Ok(Box::new(four::DayFour::default())),
         _ => Err(format!("Day {} not supported.", day)),
     }
 }
