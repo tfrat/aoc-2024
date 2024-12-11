2b5430f155b965a53f348a002b35701502bbf404
Tim Fratangelo <tfrat84@gmail.com>
2024-12-10 22:31:37 -0500

    Day 9 part 2 attempt

--- src/days/nine.rs
+++ src/days/nine.rs
@@ -1,4 +1,5 @@
 use crate::days::Day;
+use std::ops::Range;
 
 #[derive(Default)]
 pub struct DayNine {}
@@ -45,6 +46,28 @@ impl DayNine {
         (total_file_size, out)
     }
 
+    fn load_files_and_slice_locations(fs: &str) -> (Vec<Range<usize>>, Vec<Range<usize>>) {
+        let (file_slices, space_slices, _) = fs
+            .chars()
+            .map(|c| c.to_string().parse::<u32>().unwrap())
+            .enumerate()
+            .fold(
+                (Vec::new(), Vec::new(), 0),
+                |(mut files, mut spaces, start), (i, num)| {
+                    let end = start + num as usize;
+                    if end > start {
+                        match i % 2 {
+                            0 => files.push(start..end),
+                            1 => spaces.push(start..end),
+                            _ => (),
+                        }
+                    }
+                    (files, spaces, end)
+                },
+            );
+        (file_slices, space_slices)
+    }
+
     fn defrag(total_file_size: &u32, disk_map: &[Option<u32>]) -> u64 {
         let mut next_file = disk_map.iter().rev().flatten();
 
@@ -58,23 +81,31 @@ impl DayNine {
             .sum()
     }
 
-    fn contiguous_defrag(total_file_size: &u32, disk_map: &[Option<u32>]) -> u64 {
-        // let mut defragged = disk_map.clone();
-        // let mut next_file = defragged.
-        //     iter()
-        //     .rev()
-        //     .flatten()
-        //     .enumerate()
-        //     .zip(defragged.iter().rev().flatten().skip(1))
-        //     .scan(Vec::new(), |positions, (i, current), next|
-        //         if positions.is_empty() {
-        //             positions.push((i as u32, current));
-        //
-        //         }
-        //     )
-        // defragged.iter_
-        // todo
-        (*total_file_size + disk_map.len() as u32) as u64
+    fn contiguous_defrag(
+        files: &[Range<usize>],
+        mut spaces: Vec<Range<usize>>,
+        disk_map: &mut [Option<u32>],
+    ) {
+        files.iter().rev().for_each(|file_range| {
+            if let Some((space_range_index, space_range)) = spaces
+                .clone()
+                .iter()
+                .enumerate()
+                .find(|(_, space)| space.len() >= file_range.len())
+            {
+                for (file_index, space_index) in file_range.clone().zip(space_range.clone()) {
+                    disk_map[space_index] = disk_map[file_index];
+                    disk_map[file_index] = None;
+                }
+                spaces.remove(space_range_index);
+                if space_range.len() > file_range.len() {
+                    spaces.insert(
+                        space_range_index,
+                        space_range.start + file_range.len()..space_range.end,
+                    )
+                }
+            }
+        })
     }
 }
 
@@ -85,8 +116,17 @@ impl Day for DayNine {
     }
 
     fn part_two(&self, input: &str) -> String {
-        let (total_file_size, disk_map) = DayNine::load_diskmap(input);
-        DayNine::contiguous_defrag(&total_file_size, &disk_map).to_string()
+        let (_, ref mut disk_map) = DayNine::load_diskmap(input);
+        let (files, spaces) = DayNine::load_files_and_slice_locations(input);
+        // too high: 8800549860065
+        DayNine::contiguous_defrag(&files, spaces, disk_map);
+        disk_map
+            .iter()
+            .enumerate()
+            .filter(|(_, number)| number.is_some())
+            .map(|(i, number)| i as u64 * number.unwrap() as u64)
+            .sum::<u64>()
+            .to_string()
     }
 }
 
