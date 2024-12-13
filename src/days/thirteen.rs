17b55ee2b022f32b868d80971f4060529a5cfd67
Tim Fratangelo <tfrat84@gmail.com>
2024-12-13 18:07:21 -0500

    Day 13 part 2

--- src/days/thirteen.rs
+++ src/days/thirteen.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Button {
-    cost: u32,
+    cost: u64,
     moves: Coord,
 }
 
@@ -62,16 +62,15 @@ impl DayThirteen {
     fn run(
         prize_machine: &PrizeMachine,
         location: &Coord,
-        a_presses: u32,
-        b_presses: u32,
-        press_limit: &Option<u32>,
-        memo: &mut HashMap<Coord, Option<u32>>,
-    ) -> Option<u32> {
+        a_presses: u64,
+        b_presses: u64,
+        memo: &mut HashMap<Coord, Option<u64>>,
+    ) -> Option<u64> {
         if let Some(cost) = memo.get(location) {
             return *cost;
         }
 
-        if press_limit.is_some_and(|limit| a_presses > limit && b_presses > limit)
+        if a_presses > 100 && b_presses > 100
             || location.x > prize_machine.prize_location.x
             || location.y > prize_machine.prize_location.y
         {
@@ -83,23 +82,9 @@ impl DayThirteen {
         }
 
         let next_a = location.plus(prize_machine.a.moves.x, prize_machine.a.moves.y);
-        let a_cost = Self::run(
-            prize_machine,
-            &next_a,
-            a_presses + 1,
-            b_presses,
-            press_limit,
-            memo,
-        );
+        let a_cost = Self::run(prize_machine, &next_a, a_presses + 1, b_presses, memo);
         let next_b = location.plus(prize_machine.b.moves.x, prize_machine.b.moves.y);
-        let b_cost = Self::run(
-            prize_machine,
-            &next_b,
-            a_presses,
-            b_presses + 1,
-            press_limit,
-            memo,
-        );
+        let b_cost = Self::run(prize_machine, &next_b, a_presses, b_presses + 1, memo);
 
         let cost = match (a_cost, b_cost) {
             (Some(a), None) => Some(a + prize_machine.a.cost),
@@ -111,32 +96,54 @@ impl DayThirteen {
         memo.insert(*location, cost);
         cost
     }
-    fn get_prizes(machines: &[PrizeMachine], press_limit: &Option<u32>) -> u32 {
+    fn get_prizes(machines: &[PrizeMachine]) -> u64 {
         machines
             .iter()
-            .filter_map(|machine| {
-                Self::run(
-                    machine,
-                    &Coord::new(0, 0),
-                    0,
-                    0,
-                    press_limit,
-                    &mut HashMap::new(),
-                )
-            })
+            .filter_map(|machine| Self::run(machine, &Coord::new(0, 0), 0, 0, &mut HashMap::new()))
             .sum()
+    }
+
+    fn calculate_cost(prize_machine: &PrizeMachine) -> Option<u64> {
+        let x1 = prize_machine.a.moves.x;
+        let x2 = prize_machine.b.moves.x;
+        let z1 = prize_machine.prize_location.x;
+
+        let y1 = prize_machine.a.moves.y;
+        let y2 = prize_machine.b.moves.y;
+        let z2 = prize_machine.prize_location.y;
+
+        let b_denom = -y2 * x1 + x2 * y1;
+
+        if b_denom == 0 || x1 == 0 {
+            return None;
+        }
+
+        let b = (y1 * z1 - z2 * x1) / b_denom;
+        let a = (z1 - b * x2) / x1;
+
+        let destination = Coord::new(a * x1 + b * x2, a * y1 + b * y2);
+
+        if destination != prize_machine.prize_location {
+            return None;
+        }
+
+        Some((a * 3 + b) as u64)
+    }
+
+    fn get_prizes_fast(prize_machines: &[PrizeMachine]) -> u64 {
+        prize_machines.iter().flat_map(Self::calculate_cost).sum()
     }
 }
 
 impl Day for DayThirteen {
     fn part_one(&self, input: &str) -> String {
         let machines = DayThirteen::parse_machines(input, 0);
-        DayThirteen::get_prizes(&machines, &Some(100)).to_string()
+        DayThirteen::get_prizes(&machines).to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
         let machines = DayThirteen::parse_machines(input, 10000000000000);
-        DayThirteen::get_prizes(&machines, &None).to_string()
+        DayThirteen::get_prizes_fast(&machines).to_string()
     }
 }
 
@@ -189,7 +196,7 @@ Prize: X=7870, Y=6450
 Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279"#,
-            480,
+            875318608908u64,
         )];
         for (input, expected) in cases {
             assert_eq!(day.part_two(input), expected.to_string())
